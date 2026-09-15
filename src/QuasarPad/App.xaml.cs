3b4be4c8ab2c8d92f955021feb89c746b02a459b
freedomania <nftgamingplay@gmail.com>
2026-09-15 16:50:12 +0700

    Add App, Themes and core structure

---
+++ src/QuasarPad/App.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+
+namespace QuasarPad
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // Future: Load settings, apply theme, handle command line args
+        }
+    }
+}
